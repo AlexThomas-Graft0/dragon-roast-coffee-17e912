cdacc0e34c7f6e7ac1e98b197323b0b6c4b60d16
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2026-07-04 17:14:13 +0100

    add app/protected/page.tsx

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -15,7 +15,7 @@ async function UserDetails() {
   return JSON.stringify(data.user, null, 2);
 }
 
-export default function ProtectedPage() {
+export default async function ProtectedPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
       <div className="w-full">
@@ -28,7 +28,7 @@ export default function ProtectedPage() {
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto w-full">
-          <Suspense>
+          <Suspense fallback={<div>Loading...</div>}>
             <UserDetails />
           </Suspense>
         </pre>
